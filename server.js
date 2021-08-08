357c2c2895e4f0a229b9a8bb722cfa64f4bbca51
jpahme <jpahme@yahoo.com>
2021-08-08 18:34:40 -0400

    .env variables

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const cors = require("cors");
 const axios = require("axios");
 const bodyParser = require("body-parser");
